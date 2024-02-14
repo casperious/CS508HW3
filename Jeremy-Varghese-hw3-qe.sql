d3e4a54a5a2bccc2cbe46f9f1afb6078a46e3c2e
Jeremy Varghese <35908976+casperious@users.noreply.github.com>
2024-02-14 14:29:58 -0500

    Closer

--- Jeremy-Varghese-hw3-qe.sql
+++ Jeremy-Varghese-hw3-qe.sql
@@ -1,3 +1,4 @@
-select 'JV85',name,semester,year, time_slot_id, count((day,start_hr,start_min))
-from (((instructor natural join teaches) natural join section) natural join time_slot) as t1
-group by name,semester,year,time_slot_id
+select name,semester,year,day,start_hr,count((semester,year,day,start_hr,start_min))
+from ((instructor natural join teaches) natural join section) natural join time_slot
+group by name,semester,year,day,start_hr
+having count((semester,year,day,start_hr,start_min))>1
